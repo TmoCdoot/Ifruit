5a5635f64b9add42503373275edf913d1b83d429
TmoCdoot <92098125+TmoCdoot@users.noreply.github.com>
2022-03-08 08:08:28 +0100

    add form

--- src/Controller/IbigoController.php
+++ src/Controller/IbigoController.php
@@ -12,26 +12,30 @@ use App\Form\IbigoType;
 
 class IbigoController extends AbstractController
 {
-    public function index(Request $request, ManagerRegistry $doctrine):Response
+    public function index():Response
     {
-       // get the user information and notifications somehow
+        $ibigo = new IbigoProduct();
+        $form = $this->createForm(IbigoType::class, $ibigo);
 
-       // the template path is the relative file path from `templates/`
+        return $this->render('iBigo.html.twig', [
+            "form" => $form->createView()
+        ]);
+    }
 
-       $ibigo = new IbigoProduct();
-       $form = $this->createForm(IbigoType::class, $ibigo);
-
+    public function submit(Request $request, ManagerRegistry $doctrine):Response
+    {
        //traitement formuilaire
-       $form->handleRequest($request);
-       if ($form->isSubmitted() && $form->isValid()) {
+        $ibigo = new IbigoProduct();
+        $form = $this->createForm(IbigoType::class, $ibigo);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
             $em = $doctrine->getManager();
 
             $em->persist($ibigo);
             $em->flush();
-       }
+        }
 
-       return $this->render('iBigo.html.twig', [
-           "form" => $form->createView()
-       ]);
+        return $this->redirectToRoute('home');
     }
 }
